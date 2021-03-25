c3e2eca4af74aad3f10c5148dfb9a0a9b98801d7
Ezhilkumaran <ezhilkumaran1995@gmail.com>
2021-03-25 11:55:52 +0530

    s5

--- src/org/samp/Sample.java
+++ src/org/samp/Sample.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
 	System.out.println("3");
 	System.out.println("4");
 	System.out.println("5");
-	int a = 10;
-	System.out.println(a);
+	int b = 10;
+	System.out.println(b);
 	
 
 }
